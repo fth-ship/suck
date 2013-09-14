d697043bdbb7d090d35b3e58f82a70081e614b88
Kaique da Silva <kaique.developer@gmail.com>
2013-09-13 23:26:48 -0300

    refactor

---
+++ bin/handler.js
@@ -0,0 +1,125 @@
+module.exports = exports = function ( program, process ) {
+    var crawler = require('../lib');
+    var path = require('path');
+    var pack = require('../package');
+    var fs = require('fs');
+    // comparison lists
+    var output = [];
+    var usage = [];
+
+    // parse to integer
+    // @param val
+    function integer (val) {
+        return parseInt(val, 10);    
+    }
+
+    // split by comma
+    // @param val
+    function list (val) {
+        return val.split(',');
+    }
+
+    // main function to run the crawler
+    function findPatternsAndLog ( program ) {
+        var options = {
+            // target of exploration
+            target: program.target,
+            // pattern list to match
+            pattern: program.list
+        };
+        // limit of deep extraction
+        var limit = program.end || 100;
+        // output filename
+        var out = program.out || 'output.json';
+        // error filename
+        var err = program.err || 'error.json';
+        // stream of file
+        var outputStream = program.out && fs.createWriteStream( out ) || null;
+
+        // delete var and exit the process
+        function end () {
+            delete options;
+            delete limit;
+            delete out;
+            delete err;
+            delete outputStream;
+            return process.exit( 0 );
+        }
+
+        // handler to start up the crawling action 
+        function crawlerHandler ( e, stack, res ) {
+            if ( e ) {
+                fs.writeFileSync( 
+                    path.join( err ), 
+                    JSON.stringify( e ),
+                    'utf-8'
+                );
+                console.log('Sorry, but something is wrong: %s', err);
+                end();
+            } else if ( program.out ) {
+                outputStream.write( JSON.stringify( output ) );
+                outputStream.close();
+                console.log('Completed: %s', out );
+                end();
+            } else {
+                console.log( JSON.stringify( output, null, 4 ) );    
+                end();
+            } 
+        }
+
+        // handler to deep extraction
+        function crawlerRecursionHandler (e, stack, res ) {
+            // handler of anchors matched
+            function stackHandler ( href ) {
+                usage.push( href );
+                if ( output.indexOf( href ) === -1 ) {
+                    output.push( href ); 
+                    if ( program.verbose ) {
+                        console.log(
+                            '[ %s ] - %s - total: %s', 
+                            res.status, 
+                            href, 
+                            output.length
+                        );
+                    }
+                }
+            }
+
+            // handler of step by step
+            function recursionHandler () {
+                stack.map( stackHandler );
+                limit -= 1;
+                options['target'] = usage[0];
+                usage.shift();
+                crawler.pattern.search( options, crawlerRecursionHandler );
+            }
+
+            // terminal handler
+            function finalHandler () {
+                crawlerHandler( e, stack, res );
+            }
+
+            if ( limit ) { 
+                recursionHandler();    
+            } else {
+                finalHandler();
+            }
+        }
+        crawler.pattern.search( options, crawlerRecursionHandler );
+    }
+
+    program
+        .version( pack['version'] )
+        .usage('[options]')
+        .option('-t, --target [host]', 'Target of search')
+        .option('-l, --list <patterns>', 'Patterns to match', list)
+        .option('-e, --end <n>', 'Limit of exploration', integer)
+        .option('-o, --out [file]', 'Out filename')
+        .option('-e, --err [file]', 'Error filename')
+        .option('-v, --verbose', 'Show progress in the stdout')
+        .parse(process.argv);
+
+    if ( program.target && program.list ) {
+        findPatternsAndLog(program);    
+    }
+};
--- bin/index.js
+++ bin/index.js
@@ -1,110 +1,15 @@
 #!/usr/bin/env node
 'use stict';
+
+/* * * * * * * * * * 
+ *                 *
+ *  Dependencies   *
+ *                 *
+ * * * * * * * * * */
+
 var program = require('commander');
-var crawler = require('../lib');
-var path = require('path');
-var pack = require('../package');
-var fs = require('fs');
+var programHandler = require('./handler');
 
-// comparison lists
-var output = [];
-var usage = [];
-
-function integer (val) {
-    return parseInt(val, 10);    
-}
-
-function list (val) {
-    return val.split(',');
-}
-
-function findPatternsAndLog ( program ) {
-    var options = {
-        target: program.target,
-        pattern: program.list
-    };
-    var limit = program.end || 100;
-    var out = program.out || 'output.json';
-    var err = program.err || 'error.json';
-    var outputStream = program.out && fs.createWriteStream( out ) || null;
-
-    function end () {
-        delete options;
-        delete limit;
-        delete out;
-        delete err;
-        delete outputStream;
-        return process.exit( 0 );
-    }
-
-    function crawlerHandler ( e, stack, res ) {
-        if ( e ) {
-            fs.writeFileSync( 
-                path.join( err ), 
-                JSON.stringify( e ),
-                'utf-8'
-            );
-            console.log('Sorry, but something is wrong: %s', err);
-            end();
-        } else if ( program.out ) {
-            outputStream.write( JSON.stringify( output ) );
-            outputStream.close();
-            console.log('Completed: %s', out );
-            end();
-        } else {
-            console.log( JSON.stringify( output, null, 4 ) );    
-            end();
-        } 
-    }
-
-    function crawlerRecursionHandler (e, stack, res ) {
-        function stackHandler ( href ) {
-            usage.push( href );
-            if ( output.indexOf( href ) === -1 ) {
-                output.push( href ); 
-                if ( program.verbose ) {
-                    console.log(
-                        '[ %s ] - %s - total: %s', 
-                        res.status, 
-                        href, 
-                        output.length
-                    );
-                }
-            }
-        }
-
-        function recursionHandler () {
-            stack.map( stackHandler );
-            limit -= 1;
-            options['target'] = usage[0];
-            usage.shift();
-            crawler.pattern.search( options, crawlerRecursionHandler );
-        }
-
-        function finalHandler () {
-            crawlerHandler( e, stack, res );
-        }
-
-        if ( limit ) { 
-            recursionHandler();    
-        } else {
-            finalHandler();
-        }
-    }
-    crawler.pattern.search( options, crawlerRecursionHandler );
-}
-
-program
-    .version( pack['version'] )
-    .usage('[options]')
-    .option('-t, --target [host]', 'Target of search')
-    .option('-l, --list <patterns>', 'Patterns to match', list)
-    .option('-e, --end <n>', 'Limit of exploration', integer)
-    .option('-o, --out [file]', 'Out filename')
-    .option('-e, --err [file]', 'Error filename')
-    .option('-v, --verbose', 'Show progress in the stdout')
-    .parse(process.argv);
-
-if ( program.target && program.list ) {
-    findPatternsAndLog(program);    
-}
+// run the handlers
+// @param program
+programHandler( program, process );
